9580fcca171e8318681f1d5c349f2f080c43472f
Liu-ZY-98 <51340403+Liu-ZY-98@users.noreply.github.com>
2022-07-22 22:43:31 -0700

    Add files via upload

---
+++ Lab 3/25105882_lab3v2.s
@@ -0,0 +1,753 @@
+jal main
+#                         ICS 51, Lab #2
+#
+#      IMPORTANT NOTES:
+#
+#      Write your assembly code only in the marked blocks.
+#
+#      DO NOT change anything outside the marked blocks.
+#
+#      Remember to fill in your name, student ID in the designated fields.
+#
+#
+#      contact sajjadt@uci.edu
+###############################################################
+#                           Data Section
+.data
+student_name: .asciiz "Zhiyuan Liu"
+student_id: .asciiz "25105882"
+
+identity_m: .word 1, 0, 0, 0, 1, 0
+scale_m:    .word 2, 0, 0, 0, 1, 0
+rotation_m: .word 0, 1, 0, 1, 0, 0
+shear_m:    .word 1, 1, 0, 0, 1, 0
+
+input_1: .byte 100, 60, 81, 2
+input_2: .byte 10, 20, 30, 110, 127, 130, 210, 220, 230
+input_3: .byte 0, 10, 20, 30, 40, 110, 128, 130, 140, 210, 220, 230, 240, 250, 255, 55
+output_1: .space 4
+output_2: .space 9
+output_3: .space 16
+
+# Part 1 tests data
+# thresh value = 128
+test_11_expected_output: .byte 0, 0, 0, 0
+test_12_expected_output: .byte 0, 0, 0, 0, 0, 255, 255, 255, 255
+test_13_expected_output: .byte 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0
+
+# Part 2 tests data
+# identity and rotation on input 2
+test_221_expected_output: .byte 10, 20, 30, 110, 127, 130, 210, 220, 230
+test_222_expected_output: .byte 10, 110, 210, 20, 127, 220, 30, 130, 230
+# identity, scale, rotation, and shear on input 3
+test_231_expected_output: .byte 0, 10, 20, 30, 40, 110, 128, 130, 140, 210, 220, 230, 240, 250, 255, 55
+test_232_expected_output: .byte 0, 20, 0, 0, 40, 128, 0, 0, 140, 220, 0, 0, 240, 255, 0, 0
+test_233_expected_output: .byte 0, 40, 140, 240, 10, 110, 210, 250, 20, 128, 220, 255, 30, 130, 230, 55
+test_234_expected_output: .byte 0, 10, 20, 30, 110, 128, 130, 0, 220, 230, 0, 0, 55, 0, 0, 0
+# Messages
+new_line: .asciiz "\n"
+space: .asciiz " "
+i_str: .asciiz  "Program input:   " 
+po_str: .asciiz "Program output:  " 
+eo_str: .asciiz "Expected output: " 
+t1_str: .asciiz "Testing part 1: \n" 
+t2_str_0: .asciiz "Testing part 2 (identity): \n" 
+t2_str_1: .asciiz "Testing part 2 (scale): \n" 
+t2_str_2: .asciiz "Testing part 2 (rotation): \n" 
+t2_str_3: .asciiz "Testing part 2 (shear): \n" 
+
+# Files
+fin: .asciiz "lenna.pgm"
+fout_thresh: .asciiz "lenna_thresh.pgm"
+fout_rotate: .asciiz "lenna_rotation.pgm"
+fout_shear: .asciiz "lenna_shear.pgm"
+fout_scale: .asciiz "lenna_scale.pgm"
+
+# Input/output buffers
+.align 2
+in_buffer: .space 400000
+in_buffer_end:
+.align 2
+out_buffer: .space 400000
+out_buffer_end:
+
+###############################################################
+#                           Text Section
+.text
+# Utility function to print byte arrays
+#a0: array
+#a1: length
+print_array:
+li $t1, 0
+move $t2, $a0
+print:
+lb $a0, ($t2)
+andi $a0, $a0, 0xff
+li $v0, 1   
+syscall
+li $v0, 4
+la $a0, space
+syscall
+addi $t2, $t2, 1
+addi $t1, $t1, 1
+blt $t1, $a1, print
+jr $ra
+########################################################################################
+#a0 = input array
+#a1 = output array
+#a2 = matrix
+#s3 = input dim
+#s4 = test str
+#s5 = expected array
+# Test transform function
+########################################################################################
+test_p2:
+# save ra
+addi $sp, $sp, -4
+sw $ra, 0($sp)
+
+addi $sp, $sp, -4
+sw $a0, 0($sp)
+addi $sp, $sp, -4
+sw $a1, 0($sp)
+addi $sp, $sp, -4
+sw $a2, 0($sp)
+addi $sp, $sp, -4
+sw $a3, 0($sp)
+addi $sp, $sp, -4
+sw $s4, 0($sp)
+addi $sp, $sp, -4
+sw $s5, 0($sp)
+
+
+#a0: input buffer address
+#a1: output buffer address
+#a2: transform matrix address
+#a3: image dimension  (Image will be square sized, i.e. total size = a3*a3)
+jal transform 
+
+lw $s5, 0($sp)    
+addi $sp, $sp, 4
+lw $s4, 0($sp)
+addi $sp, $sp, 4
+lw $s3, 0($sp)
+addi $sp, $sp, 4
+lw $s2, 0($sp)
+addi $sp, $sp, 4
+lw $s1, 0($sp)
+addi $sp, $sp, 4
+lw $s0, 0($sp)
+addi $sp, $sp, 4
+
+# s5: exp arraay
+# s4: input string
+# s3: input dimenstion
+# s2: matrix
+# s1: user out
+# s0: inputd
+
+mul $s3, $s3, $s3
+
+move $a0, $s4
+syscall
+la $a0, i_str
+syscall
+move $a0, $s0
+move $a1, $s3
+jal print_array
+li $v0, 4
+la $a0, new_line
+syscall
+
+la $a0, po_str
+syscall
+move $a0, $s1
+move $a1, $s3
+jal print_array
+li $v0, 4
+la $a0, new_line
+syscall
+la $a0, eo_str
+syscall
+move $a0, $s5
+move $a1, $s3
+jal print_array
+li $v0, 4
+la $a0, new_line
+syscall
+syscall
+
+# restore ra
+lw $ra, 0($sp)
+addi $sp, $sp, 4
+
+jr $ra
+###############################################################
+###############################################################
+#                       PART 1 (Image Thresholding)
+#a0: input buffer address
+#a1: output buffer address
+#a2: image dimension (Image will be square sized, i.e., number of pixels = a3*a3)
+#a3: threshold value 
+###############################################################
+threshold:
+############################## Part 1: your code begins here ###
+move $t0, $a0
+move $t1, $a1
+li $t6, 0x00
+li $t7, 0xff
+
+mul $t2, $a2, $a2
+
+move $t3, $zero
+
+everypixel1:
+bge $t3, $t2, end1
+lbu $t5, ($t0)
+bge $t5, $a3, bigger
+sb $t6, ($t1)
+j next
+bigger:
+sb $t7, ($t1)
+
+next:
+addi $t0, $t0, 1
+addi $t1, $t1, 1
+addi $t3, $t3, 1
+j everypixel1
+
+end1:
+
+############################## Part 1: your code ends here ###
+jr $ra
+###############################################################
+###############################################################
+#                           PART 2 (Matrix Transform)
+#a0: input buffer address
+#a1: output buffer address
+#a2: transform matrix address
+#a3: image dimension  (Image will be square sized, i.e., number of pixels = a3*a3)
+###############################################################
+transform:
+############################### Part 2: your code begins here ##
+li $t0, 0
+li $t1, 0
+
+move $s6, $a0
+move $s7, $a1
+
+li $t2, 0
+li $t3, 0
+
+subi $t8, $a3, 1
+
+lw $s0, ($a2)
+lw $s1, 4($a2)
+lw $s2, 8($a2)
+lw $s3, 12($a2)
+lw $s4, 16($a2)
+lw $s5, 20($a2)
+
+getXY:
+mul $t4, $t2, $s0
+mul $t5, $t3, $s1
+add $t4, $t4, $t5
+add $t4, $t4, $s2
+
+mul $t6, $t2, $s3
+mul $t7, $t3, $s4
+add $t6, $t6, $t7
+add $t6, $t6, $s5
+
+mul $t0, $t3, $a3
+add $t0, $t0, $t2
+
+bge $t4, $a3, dark
+bge $t6, $a3, dark
+
+mul $t1, $t6, $a3
+add $t1, $t1, $t4
+j match
+
+dark:
+add $s6, $a1, $t0
+li $t5, 0x00
+sb $t5, ($s6)
+j check2
+
+match:
+add $s6, $a1, $t0
+add $s7, $a0, $t1
+lbu $t5, ($s7)
+sb $t5, ($s6)
+
+check2:
+blt $t2, $t8, inc
+move $t2, $zero
+bge $t3, $t8, end2
+addi $t3, $t3, 1
+j getXY
+
+inc:
+addi $t2, $t2, 1
+j getXY
+
+end2:
+
+############################### Part 2: your code ends here  ##
+jr $ra
+###############################################################
+
+###############################################################
+#                          Main Function
+.globl main
+main:
+
+.text
+
+li $v0, 4
+la $a0, student_name
+syscall
+la $a0, new_line
+syscall  
+la $a0, student_id
+syscall 
+la $a0, new_line
+syscall
+
+
+# Test threshold function
+li $v0, 4
+la $a0, t1_str
+syscall
+
+la $a0, input_1
+la $a1, output_1
+li $a2, 2
+li $a3, 128
+jal threshold
+
+la $a0, i_str
+syscall
+la $a0, input_1
+li $a1, 4
+jal print_array
+li $v0, 4
+la $a0, new_line
+syscall
+
+la $a0, po_str
+syscall
+la $a0, output_1
+li $a1, 4
+jal print_array
+li $v0, 4
+la $a0, new_line
+syscall
+
+la $a0, eo_str
+syscall
+la $a0, test_11_expected_output
+li $a1, 4
+jal print_array
+li $v0, 4
+la $a0, new_line
+syscall
+syscall
+
+la $a0, input_2
+la $a1, output_2
+li $a2, 3
+li $a3, 128
+jal threshold
+
+la $a0, i_str
+syscall
+la $a0, input_2
+li $a1, 9
+jal print_array
+li $v0, 4
+la $a0, new_line
+syscall
+
+la $a0, po_str
+syscall
+la $a0, output_2
+li $a1, 9
+jal print_array
+li $v0, 4
+la $a0, new_line
+syscall
+
+la $a0, eo_str
+syscall
+la $a0, test_12_expected_output
+li $a1, 9
+jal print_array
+li $v0, 4
+la $a0, new_line
+syscall
+syscall
+
+la $a0, input_3
+la $a1, output_3
+li $a2, 4
+li $a3, 128
+jal threshold
+
+la $a0, i_str
+syscall
+la $a0, input_3
+li $a1, 16
+jal print_array
+li $v0, 4
+la $a0, new_line
+syscall
+
+la $a0, po_str
+syscall
+la $a0, output_3
+li $a1, 16
+jal print_array
+li $v0, 4
+la $a0, new_line
+syscall
+
+la $a0, eo_str
+syscall
+la $a0, test_13_expected_output
+li $a1, 16
+jal print_array
+li $v0, 4
+la $a0, new_line
+syscall
+syscall
+
+# Part 2 testing
+#a0 = input array
+#a1 = output array
+#a2 = matrix
+#s3 = input dim
+#s4 = test str
+#s5 = expected array
+
+la $a0, input_2
+la $a1, output_2
+la $a2, identity_m
+li $a3, 3 # dim
+la $s4, t2_str_0
+la $s5, test_221_expected_output
+jal test_p2
+
+la $a0, input_2
+la $a1, output_2
+la $a2, rotation_m
+li $a3, 3 # dim
+la $s4, t2_str_2
+la $s5, test_222_expected_output
+jal test_p2
+
+########
+la $a0, input_3
+la $a1, output_3
+la $a2, identity_m
+li $a3, 4 # dim
+la $s4, t2_str_0
+la $s5, test_231_expected_output
+jal test_p2
+
+la $a0, input_3
+la $a1, output_3
+la $a2, scale_m
+li $a3, 4 # dim
+la $s4, t2_str_1
+la $s5, test_232_expected_output
+jal test_p2
+
+la $a0, input_3
+la $a1, output_3
+la $a2, rotation_m
+li $a3, 4 # dim
+la $s4, t2_str_2
+la $s5, test_233_expected_output
+jal test_p2
+
+la $a0, input_3
+la $a1, output_3
+la $a2, shear_m
+li $a3, 4 # dim
+la $s4, t2_str_3
+la $s5, test_234_expected_output
+jal test_p2
+
+
+#### Test on images
+#open the file for writing
+li   $v0, 13       # system call for open file
+la   $a0, fin      # board file name
+li   $a1, 0        # Open for reading
+li   $a2, 0
+syscall            # open a file (file descriptor returned in $v0)
+move $s6, $v0      # save the file descriptor
+
+#read from file
+li   $v0, 14       # system call for read from file
+move $a0, $s6      # file descriptor
+la   $a1, in_buffer   # address of buffer to which to read
+la   $a2, in_buffer_end     # hardcoded buffer length
+sub $a2, $a2, $a1
+syscall            # read from file
+
+# Close the file
+li   $v0, 16       # system call for close file
+move $a0, $s6      # file descriptor to close
+syscall
+
+## Copy the header
+la $t0, in_buffer
+la $t1, out_buffer
+lw $t2, ($t0)
+sw $t2, ($t1)
+lw $t2, 4($t0)
+sw $t2, 4($t1)
+lw $t2, 8($t0)
+sw $t2, 8($t1)
+lw $t2, 12($t0)
+sw $t2, 12($t1)
+
+# Threshold
+la $a0, in_buffer
+addi $a0, $a0, 16
+la $a1, out_buffer
+addi $a1, $a1, 16
+li $a2, 512
+li $a3, 80
+jal threshold 
+
+
+#open a file for writing
+li   $v0, 13       # system call for open file
+la   $a0, fout_thresh      # board file name
+li   $a1, 1        # Open for writing
+li   $a2, 0
+syscall            # open a file (file descriptor returned in $v0)
+move $s6, $v0      # save the file descriptor
+# write back
+li   $v0, 15       # system call for read from file
+move $a0, $s6      # file descriptor
+la   $a1, out_buffer   # address of buffer to which to read
+la   $a2, out_buffer_end     # hardcoded buffer length
+subu $a2, $a2, $a1
+syscall            # read from file
+
+# Close the file
+li   $v0, 16       # system call for close file
+move $a0, $s6      # file descriptor to close
+syscall    
+
+
+#open the file for writing
+li   $v0, 13       # system call for open file
+la   $a0, fin      # board file name
+li   $a1, 0        # Open for reading
+li   $a2, 0
+syscall            # open a file (file descriptor returned in $v0)
+move $s6, $v0      # save the file descriptor
+
+#read from file
+li   $v0, 14       # system call for read from file
+move $a0, $s6      # file descriptor
+la   $a1, in_buffer   # address of buffer to which to read
+la   $a2, in_buffer_end     # hardcoded buffer length
+sub $a2, $a2, $a1
+syscall            # read from file
+
+# Close the file
+li   $v0, 16       # system call for close file
+move $a0, $s6      # file descriptor to close
+syscall
+
+
+
+## Copy the header
+la $t0, in_buffer
+la $t1, out_buffer
+lw $t2, ($t0)
+sw $t2, ($t1)
+lw $t2, 4($t0)
+sw $t2, 4($t1)
+lw $t2, 8($t0)
+sw $t2, 8($t1)
+lw $t2, 12($t0)
+sw $t2, 12($t1)
+
+# Rotate
+la $a0, in_buffer
+addi $a0, $a0, 16
+la $a1, out_buffer
+addi $a1, $a1, 16
+la $a2, rotation_m
+li $a3, 512
+jal transform 
+
+
+#open a file for writing
+li   $v0, 13       # system call for open file
+la   $a0, fout_rotate      # board file name
+li   $a1, 1        # Open for writing
+li   $a2, 0
+syscall            # open a file (file descriptor returned in $v0)
+move $s6, $v0      # save the file descriptor
+# write back
+li   $v0, 15       # system call for read from file
+move $a0, $s6      # file descriptor
+la   $a1, out_buffer   # address of buffer to which to read
+la   $a2, out_buffer_end     # hardcoded buffer length
+subu $a2, $a2, $a1
+syscall            # read from file
+
+# Close the file
+li   $v0, 16       # system call for close file
+move $a0, $s6      # file descriptor to close
+syscall
+
+
+
+#open the file for writing
+li   $v0, 13       # system call for open file
+la   $a0, fin      # board file name
+li   $a1, 0        # Open for reading
+li   $a2, 0
+syscall            # open a file (file descriptor returned in $v0)
+move $s6, $v0      # save the file descriptor
+
+#read from file
+li   $v0, 14       # system call for read from file
+move $a0, $s6      # file descriptor
+la   $a1, in_buffer   # address of buffer to which to read
+la   $a2, in_buffer_end     # hardcoded buffer length
+sub $a2, $a2, $a1
+syscall            # read from file
+
+# Close the file
+li   $v0, 16       # system call for close file
+move $a0, $s6      # file descriptor to close
+syscall
+
+
+
+## Copy the header
+la $t0, in_buffer
+la $t1, out_buffer
+lw $t2, ($t0)
+sw $t2, ($t1)
+lw $t2, 4($t0)
+sw $t2, 4($t1)
+lw $t2, 8($t0)
+sw $t2, 8($t1)
+lw $t2, 12($t0)
+sw $t2, 12($t1)
+
+# Shear
+la $a0, in_buffer
+addi $a0, $a0, 16
+la $a1, out_buffer
+addi $a1, $a1, 16
+la $a2, shear_m
+li $a3, 512
+jal transform 
+
+
+#open a file for writing
+li   $v0, 13       # system call for open file
+la   $a0, fout_shear      # board file name
+li   $a1, 1        # Open for writing
+li   $a2, 0
+syscall            # open a file (file descriptor returned in $v0)
+move $s6, $v0      # save the file descriptor
+# write back
+li   $v0, 15       # system call for read from file
+move $a0, $s6      # file descriptor
+la   $a1, out_buffer   # address of buffer to which to read
+la   $a2, out_buffer_end     # hardcoded buffer length
+subu $a2, $a2, $a1
+syscall            # read from file
+
+# Close the file
+li   $v0, 16       # system call for close file
+move $a0, $s6      # file descriptor to close
+syscall
+
+
+
+
+#open the file for writing
+li   $v0, 13       # system call for open file
+la   $a0, fin      # board file name
+li   $a1, 0        # Open for reading
+li   $a2, 0
+syscall            # open a file (file descriptor returned in $v0)
+move $s6, $v0      # save the file descriptor
+
+#read from file
+li   $v0, 14       # system call for read from file
+move $a0, $s6      # file descriptor
+la   $a1, in_buffer   # address of buffer to which to read
+la   $a2, in_buffer_end     # hardcoded buffer length
+sub $a2, $a2, $a1
+syscall            # read from file
+
+# Close the file
+li   $v0, 16       # system call for close file
+move $a0, $s6      # file descriptor to close
+syscall
+
+
+
+## Copy the header
+la $t0, in_buffer
+la $t1, out_buffer
+lw $t2, ($t0)
+sw $t2, ($t1)
+lw $t2, 4($t0)
+sw $t2, 4($t1)
+lw $t2, 8($t0)
+sw $t2, 8($t1)
+lw $t2, 12($t0)
+sw $t2, 12($t1)
+
+# scale
+la $a0, in_buffer
+addi $a0, $a0, 16
+la $a1, out_buffer
+addi $a1, $a1, 16
+la $a2, scale_m
+li $a3, 512
+jal transform 
+
+
+#open a file for writing
+li   $v0, 13       # system call for open file
+la   $a0, fout_scale      # board file name
+li   $a1, 1        # Open for writing
+li   $a2, 0
+syscall            # open a file (file descriptor returned in $v0)
+move $s6, $v0      # save the file descriptor
+# write back
+li   $v0, 15       # system call for read from file
+move $a0, $s6      # file descriptor
+la   $a1, out_buffer   # address of buffer to which to read
+la   $a2, out_buffer_end     # hardcoded buffer length
+subu $a2, $a2, $a1
+syscall            # read from file
+
+# Close the file
+li   $v0, 16       # system call for close file
+move $a0, $s6      # file descriptor to close
+syscall
+
+
+_end_program:
+# end program
+li $v0, 10
+syscall
